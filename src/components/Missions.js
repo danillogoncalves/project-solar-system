b7f8a4613d94d2dbb459e143cdcf5504421ce666
danillo <danillogonvalves001@gmail.com>
2022-02-18 19:55:43 -0300

    Requisito 7 e 8 feitos.

---
+++ src/components/Missions.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Title from './Title';
+
+class Missions extends Component {
+  render() {
+    return (
+      <div data-testid="missions">
+        <Title headline="Missões" />
+      </div>
+    );
+  }
+}
+
+export default Missions;
